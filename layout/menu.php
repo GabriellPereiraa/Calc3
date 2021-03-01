4fb948eaeccf9d8a0c2a1c96b26c4b54acdeb40a
Gabriel Pereira <78962152+GabriellPereiraa@users.noreply.github.com>
2021-03-01 10:30:02 -0300

    Update menu.php

--- layout/menu.php
+++ layout/menu.php
@@ -26,98 +26,40 @@
                         <li class="sidebar-item has-sub">
                             <a href="" class="sidebar-link">
                                 <i data-feather="home" width="20"></i>
-                                <span>1 - Conhecendo a interface do Calc</span>
+                                <span>1 – Formatações Condicionais, Filtros e Classificação</span>
                             </a>
                             <ul class="submenu">
                                 <li class="sidebar-item">
                                     <a href="Topico01.php" class="sidebar-sub">
                                         <i data-feather="home" width="20"></i>
-                                        <span>1.1 - Suíte LibreOffice</span>
+                                        <span>1.1 - Formatação Condicional</span>
                                     </a>
                                 </li>
                                 <li class="sidebar-item">
                                     <a href="Topico01.2.php" class="sidebar-sub">
                                         <i data-feather="home" width="20"></i>
-                                        <span>1.2 –  O que é o Calc?</span>
+                                        <span>1.2 - Filtros</span>
                                     </a>
                                 </li>
                                 <li class="sidebar-item">
                                     <a href="Topico01.3.php" class="sidebar-sub">
                                         <i data-feather="home" width="20"></i>
-                                        <span>1.3 - Instalação da Suíte LibreOffice</span>
+                                        <span>1.3 - Classificação</span>
                                     </a>
                                 </li>
                                 <li class="sidebar-item">
                                     <a href="Topico01.4.php" class="sidebar-sub">
                                         <i data-feather="home" width="20"></i>
-                                        <span>1.4 - Elementos básicos de uma planilha no Calc</span>
-                                    </a>
-                                </li>
-                                <li class="sidebar-item">
-                                    <a href="Topico01.5.php" class="sidebar-sub">
-                                        <i data-feather="home" width="20"></i>
-                                        <span>1.5 - Interface do Calc</span>
+                                        <span>1.4 - Divisão e Congelamento de Janelas</span>
                                     </a>
                                 </li>
                             </ul>
                         </li>
-                        <li class="sidebar-item has-sub">
-                            <a href="" class='sidebar-link'>
+                        <li class="sidebar-item">
+                            <a href="Topico02.php" class="sidebar-link">
                                 <i data-feather="home" width="20"></i>
-                                <span>2 - Trabalhando com células</span>
+                                <span>2 - Gráficos</span>
                             </a>
-                            <ul class="submenu">
-                                <li class="sidebar-item">
-                                    <a href="Topico02.php" class="sidebar-sub">
-                                        <i data-feather="home" width="20"></i>
-                                        <span>2.1 - Intervalo de Célula</span>
-                                    </a>
-                                </li>
-                                <li class="sidebar-item">
-                                    <a href="Topico02.2.php" class="sidebar-sub">
-                                        <i data-feather="home" width="20"></i>
-                                        <span>2.2 - Seleção de células, colunas, linhas e folhas</span>
-                                    </a>
-                                </li>
-                                <li class="sidebar-item">
-                                    <a href="Topico02.3.php" class="sidebar-sub">
-                                        <i data-feather="home" width="20"></i>
-                                        <span>2.3 - Inserção de colunas ou linhas</span>
-                                    </a>
-                                </li>
-                                <li class="sidebar-item">
-                                    <a href="Topico02.4.php" class="sidebar-sub">
-                                        <i data-feather="home" width="20"></i>
-                                        <span>2.4 - Exclusão de colunas ou linhas</span>
-                                    </a>
-                                </li>
-                                <li class="sidebar-item">
-                                    <a href="Topico02.5.php" class="sidebar-sub">
-                                        <i data-feather="home" width="20"></i>
-                                        <span>2.5 - Trabalho com folhas de cálculo</span>
-                                    </a>
-                                </li>
-                            </ul>
-                        </li>
-                        <li class="sidebar-item has-sub">
-                            <a href="" class='sidebar-link'>
-                                <i data-feather="home" width="20"></i>
-                                <span>3 - Sintaxe, formatações e comandos</span>
-                            </a>
-                            <ul class="submenu">
-                                <li class="sidebar-item">
-                                    <a href="Topico03.php" class="sidebar-sub">
-                                        <i data-feather="home" width="20"></i>
-                                        <span>3.1 - Sintaxe</span>
-                                    </a>
-                                </li>
-                                <li class="sidebar-item">
-                                    <a href="Topico03.2.php" class="sidebar-sub">
-                                        <i data-feather="home" width="20"></i>
-                                        <span>3.2 - Formatação de dados</span>
-                                    </a>
-                                </li>
-                            </ul>
                         </li>
                         <li class="sidebar-item">
                             <a href="Referencias.php" class="sidebar-link">
